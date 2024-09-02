d4ac61409be51968f0a5841bd4708e6502a1a98f
Darby Johnston <darbyjohnston@yahoo.com>
2024-09-02 14:08:47 -0700

    Build fix

--- lib/dtk/core/FileUnix.cpp
+++ lib/dtk/core/FileUnix.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2024 Darby Johnston
 // All rights reserved.
 
-#include <dtkCore/File.h>
+#include <dtk/core/File.h>
 
 #if defined(__APPLE__)
 #include <ApplicationServices/ApplicationServices.h>
@@ -19,73 +19,70 @@
 
 namespace dtk
 {
-    namespace core
+    std::filesystem::path getUserPath(UserPath value)
     {
-        std::filesystem::path getUserPath(UserPath value)
-        {
-            std::filesystem::path out;
+        std::filesystem::path out;
 #if defined(__APPLE__)
-            OSType folderType = kDesktopFolderType;
+        OSType folderType = kDesktopFolderType;
+        switch (value)
+        {
+        case UserPath::Home:      folderType = kCurrentUserFolderType; break;
+        case UserPath::Desktop:   folderType = kDesktopFolderType;     break;
+        case UserPath::Documents: folderType = kDocumentsFolderType;   break;
+        case UserPath::Downloads: folderType = kCurrentUserFolderType; break;
+        default: break;
+        }
+        FSRef ref;
+        FSFindFolder(kUserDomain, folderType, kCreateFolder, &ref);
+        char path[PATH_MAX];
+        FSRefMakePath(&ref, (UInt8*)&path, PATH_MAX);
+        if (UserPath::Downloads == value)
+        {
+            out = std::filesystem::path(path) / std::filesystem::path("Downloads");
+        }
+        else
+        {
+            out = path;
+        }
+#else // __APPLE__
+        if (struct passwd* buf = ::getpwuid(::getuid()))
+        {
             switch (value)
             {
-            case UserPath::Home:      folderType = kCurrentUserFolderType; break;
-            case UserPath::Desktop:   folderType = kDesktopFolderType;     break;
-            case UserPath::Documents: folderType = kDocumentsFolderType;   break;
-            case UserPath::Downloads: folderType = kCurrentUserFolderType; break;
+            case UserPath::Home:
+                out = std::filesystem::path(buf->pw_dir);
+                break;
+            case UserPath::Desktop:
+                out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Desktop");
+                break;
+            case UserPath::Documents:
+                out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Documents");
+                break;
+            case UserPath::Downloads:
+                out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Downloads");
+                break;
             default: break;
             }
-            FSRef ref;
-            FSFindFolder(kUserDomain, folderType, kCreateFolder, &ref);
-            char path[PATH_MAX];
-            FSRefMakePath(&ref, (UInt8*)&path, PATH_MAX);
-            if (UserPath::Downloads == value)
-            {
-                out = std::filesystem::path(path) / std::filesystem::path("Downloads");
-            }
-            else
-            {
-                out = path;
-            }
-#else // __APPLE__
-            if (struct passwd* buf = ::getpwuid(::getuid()))
-            {
-                switch (value)
-                {
-                case UserPath::Home:
-                    out = std::filesystem::path(buf->pw_dir);
-                    break;
-                case UserPath::Desktop:
-                    out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Desktop");
-                    break;
-                case UserPath::Documents:
-                    out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Documents");
-                    break;
-                case UserPath::Downloads:
-                    out = std::filesystem::path(buf->pw_dir) / std::filesystem::path("Downloads");
-                    break;
-                default: break;
-                }
-            }
-#endif // __APPLE__
-            return out;
         }
+#endif // __APPLE__
+        return out;
+    }
 
-        std::vector<std::filesystem::path> getDrives()
-        {
-            std::vector<std::filesystem::path> out;
-            out.push_back(std::filesystem::path("/"));
+    std::vector<std::filesystem::path> getDrives()
+    {
+        std::vector<std::filesystem::path> out;
+        out.push_back(std::filesystem::path("/"));
 #if defined(__APPLE__)
-            for (const auto& i : std::filesystem::directory_iterator("/Volumes"))
-            {
-                out.push_back(i.path());
-            }
-#else // __APPLE__
-            for (const auto& i : std::filesystem::directory_iterator("/mnt"))
-            {
-                out.push_back(i.path());
-            }
-#endif // __APPLE__
-            return out;
+        for (const auto& i : std::filesystem::directory_iterator("/Volumes"))
+        {
+            out.push_back(i.path());
         }
+#else // __APPLE__
+        for (const auto& i : std::filesystem::directory_iterator("/mnt"))
+        {
+            out.push_back(i.path());
+        }
+#endif // __APPLE__
+        return out;
     }
 }
--- lib/dtk/core/TimeUnix.cpp
+++ lib/dtk/core/TimeUnix.cpp
@@ -2,17 +2,14 @@
 // Copyright (c) 2024 Darby Johnston
 // All rights reserved.
 
-#include <dtkCore/Time.h>
+#include <dtk/core/Time.h>
 
 #include <thread>
 
 namespace dtk
 {
-    namespace core
-	{
-        void sleep(const std::chrono::microseconds& value)
-		{
-            std::this_thread::sleep_for(value);
-        }
+    void sleep(const std::chrono::microseconds& value)
+    {
+        std::this_thread::sleep_for(value);
     }
 }
