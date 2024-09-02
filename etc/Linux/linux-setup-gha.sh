6efb748ffa3dee0b4a36612a59eeb282788dcc09
Darby Johnston <darbyjohnston@yahoo.com>
2024-09-02 14:40:42 -0700

    Refactoring

--- etc/Linux/linux-setup-gha.sh
+++ etc/Linux/linux-setup-gha.sh
@@ -5,9 +5,16 @@ set -x
 sudo apt-get update
 cmake --version
 python --version
+
 if [[ $DTK_GCOV = "ON" ]]
 then
     sudo apt-get install lcov
 fi
+
 sudo apt-get install xorg-dev libglu1-mesa-dev mesa-common-dev mesa-utils xvfb
 xvfb-run glxinfo
+
+if [[ $DTK_PYTHON = "ON" ]]
+then
+    sudo apt-get install python3.8-dev
+fi
