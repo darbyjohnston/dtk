1ef5d6154b57d146121e2b836920167ccdc6fac7
Darby Johnston <darbyjohnston@yahoo.com>
2025-01-06 10:24:17 -0800

    Update copyright

--- lib/dtk/ui/ProgressDialog.cpp
+++ lib/dtk/ui/ProgressDialog.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2024 Darby Johnston
+// Copyright (c) 2024-2025 Darby Johnston
 // All rights reserved.
 
 #include <dtk/ui/ProgressDialog.h>
