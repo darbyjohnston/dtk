e76f3496ade8f43ab27c9ab339884856c8c09024
Darby Johnston <darbyjohnston@yahoo.com>
2025-02-07 12:53:14 -0800

    Refactoring

--- etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
+++ etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
@@ -3,6 +3,17 @@ include(ExternalProject)
 set(glfw3_GIT_REPOSITORY "https://github.com/glfw/glfw.git")
 set(glfw3_GIT_TAG "3.4")
 
+set(glfw3_PATCH
+    ${CMAKE_COMMAND} -E copy_if_different
+    ${CMAKE_CURRENT_SOURCE_DIR}/glfw3-patch/src/nsgl_context.m
+    ${CMAKE_CURRENT_BINARY_DIR}/glfw3/src/glfw3/src/nsgl_context.m)
+if(dtk_glfw3_DISABLE_MACOS_APP_DELEGATE)
+    list(APPEND glfw3_PATCH
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/glfw3-patch/src/cocoa_init.m
+        ${CMAKE_CURRENT_BINARY_DIR}/glfw3/src/glfw3/src/cocoa_init.m)
+endif()
+
 set(glfw3_ARGS
     ${dtk_DEPS_ARGS}
     -DCMAKE_INSTALL_LIBDIR=lib
@@ -17,6 +28,7 @@ ExternalProject_Add(
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glfw3
     GIT_REPOSITORY ${glfw3_GIT_REPOSITORY}
     GIT_TAG ${glfw3_GIT_TAG}
+    PATCH_COMMAND ${glfw3_PATCH}
     LIST_SEPARATOR |
     CMAKE_ARGS ${glfw3_ARGS})
 
