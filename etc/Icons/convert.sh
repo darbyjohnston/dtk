dc46bd6c76e9be7ef580867534276af19cf0e4bb
Darby Johnston <darbyjohnston@yahoo.com>
2025-02-11 17:11:06 -0800

    Script fix

--- etc/Icons/convert.sh
+++ etc/Icons/convert.sh
@@ -3,6 +3,6 @@
 for f in $1/*.svg
 do
         base=$(basename -s .svg $f)
-        bin/dtkresource/Debug/dtkresource.exe $f $1/${base} ${base}_svg
+        bin/dtkresource/dtkresource $f $1/${base} ${base}_svg
 done
 
