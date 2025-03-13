1be9573ce1d818470d390f378c8037c95b9e388f
Darby Johnston <darbyjohnston@yahoo.com>
2025-03-13 14:41:24 -0700

    Style fix

--- lib/dtk/ui/AppGL.cpp
+++ lib/dtk/ui/AppGL.cpp
@@ -103,7 +103,7 @@ namespace dtk
         p.iconSystem = context->getSystem<IconSystem>();
         p.style = Style::create(context);
         p.colorStyle = ObservableValue<ColorStyle>::create(colorStyle);
-        p.customColorRoles = ObservableMap<ColorRole, Color4F>::create(getCustomColorRoles());
+        p.customColorRoles = ObservableMap<ColorRole, Color4F>::create(dtk::getCustomColorRoles());
         p.displayScale = ObservableValue<float>::create(displayScale);
         p.tooltipsEnabled = ObservableValue<bool>::create(true);
 
